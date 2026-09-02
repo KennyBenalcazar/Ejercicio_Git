f2d547eaaede86f5cbb79f22f372a96717c2ba28
KennyBenalcazar <benalcazarkenny2@gmail.com>
2026-09-02 09:24:44 -0500

    Agregar menu calculadora

--- Ejercicio_Git/Program.cs
+++ Ejercicio_Git/Program.cs
@@ -10,7 +10,31 @@ namespace Ejercicio_Git
     {
         static void Main(string[] args)
         {
+            double resultado, num1=10, num2=20;
 
+            Console.WriteLine("1. Suma");
+            Console.WriteLine("2. Resta");
+            Console.WriteLine("3. Multiplicacion");
+
+            Console.WriteLine("Elige una opcion ");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    resultado=Suma(num1, num2);
+                    Console.WriteLine($"suma es  {resultado}");
+                    break;
+                case 2:
+                    resultado = Resta(num1, num2);
+                    Console.WriteLine($"Resta es {resultado}");
+                    break;
+                case 3:
+                    resultado = Multiplicacion(num1, num2);
+                    Console.WriteLine($"Multiplicacion es {resultado}");
+                    break;
+
+            }
 
 
 
